e46e68f6a7dc9f7bd7ecff6044bea169836b690e
Anna Yastrebova <direct.yastrebova@gmail.com>
2024-06-10 17:52:46 +0200

    with React Hook Form & Yup

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,125 +1,73 @@
 import styles from './app.module.css';
-import React, { useState, useRef } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+import { yupResolver } from '@hookform/resolvers/yup';
 
 const sendFormData = (formData) => {
 	console.log(formData);
 };
-
-const validateEmail = (email) => {
-	const EMAIL_REGEXP =
-		/^(([^<>()[\].,;:\s@"]+(\.[^<>()[\].,;:\s@"]+)*)|(".+"))@(([^<>()[\].,;:\s@"]+\.)+[^<>()[\].,;:\s@"]{2,})$/iu;
-	if (!EMAIL_REGEXP.test(email)) {
-		return 'Введите корректный email адрес в формате example@mail.com';
-	}
-	return null;
-};
-
-const validatePassword = (password) => {
-	if (!/(?=.*[A-Z])(?=.*\d)^[A-Za-z\d]{8,}$/.test(password)) {
-		return 'Пароль должен состоять из латинских букв, содержать хотя бы одну заглавную букву и цифру и быть не меньше 8 символов';
-	}
-	return null;
-};
-
-const validatePasswordMatch = (password, passwordRepeat) => {
-	if (password !== passwordRepeat) {
-		return 'Пароли не совпадают';
-	}
-	return null;
-};
+const schema = yup.object({
+	email: yup
+		.string()
+		.email('Введите корректный email адрес в формате example@mail.com')
+		.required('Поле email обязательно для заполнения'),
+	password: yup
+		.string()
+		.matches(
+			/^(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$/,
+			'Пароль должен состоять из латинских букв, содержать хотя бы одну заглавную букву и цифру и быть не меньше 8 символов',
+		),
+	passwordRepeat: yup
+		.string()
+		.oneOf([yup.ref('password'), null], 'Пароли не совпадают'),
+});
 
 export const App = () => {
-	const [formData, setFormData] = useState({
-		email: '',
-		password: '',
-		passwordRepeat: '',
-	});
-
-	const [error, setError] = useState(null); // сообщение об ошибке
-
-	const onSubmit = (event) => {
-		event.preventDefault();
-		if (error) {
-			return;
-		}
-		sendFormData(formData);
-		setFormData({
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm({
+		defaultValues: {
 			email: '',
 			password: '',
 			passwordRepeat: '',
-		});
-	};
-
-	const { email, password, passwordRepeat } = formData;
-
-	const validate = (name, value) => {
-		let newError = null;
-
-		switch (name) {
-			case 'email':
-				newError = validateEmail(value);
-				break;
-			case 'password':
-				newError = validatePassword(value);
-				break;
-			case 'passwordRepeat':
-				newError = validatePasswordMatch(password, value);
-				break;
-			default:
-				break;
-		}
-
-		setError(newError);
-
-		setFormData({
-			...formData,
-			[name]: value,
-		});
-
-		if (formData.email && formData.password && formData.passwordRepeat && !newError) {
-			registerButtonRef.current.focus();
-		}
-	};
-
-	const registerButtonRef = useRef(null);
-
+		},
+		resolver: yupResolver(schema),
+	});
 	return (
 		<div className={styles.app}>
-			<form onSubmit={onSubmit}>
-				{error && <div className={styles.errorLabel}>{error}</div>}
+			<form onSubmit={handleSubmit(sendFormData)}>
+				{errors?.email?.message && (
+					<div className={styles.errorLabel}>{errors?.email?.message}</div>
+				)}
 				<input
 					name="email"
 					type="email"
 					placeholder="Почта"
-					value={email}
-					onChange={({ target }) =>
-						setFormData({ ...formData, [target.name]: target.value })
-					}
-					onBlur={({ target }) => validate(target.name, target.value)}
+					{...register('email')}
 				/>
+				{errors?.password?.message && (
+					<div className={styles.errorLabel}>{errors?.password?.message}</div>
+				)}
 				<input
 					name="password"
 					type="password"
 					placeholder="Пароль"
-					value={password}
-					onChange={({ target }) =>
-						setFormData({ ...formData, [target.name]: target.value })
-					}
-					onBlur={({ target }) => validate(target.name, target.value)}
+					{...register('password')}
 				/>
+				{errors?.passwordRepeat?.message && (
+					<div className={styles.errorLabel}>
+						{errors?.passwordRepeat?.message}
+					</div>
+				)}
 				<input
 					name="passwordRepeat"
 					type="password"
 					placeholder="Повтор пароля"
-					value={passwordRepeat}
-					onChange={({ target }) =>
-						setFormData({ ...formData, [target.name]: target.value })
-					}
-					onMouseLeave={({ target }) => validate(target.name, target.value)}
+					{...register('passwordRepeat')}
 				/>
-				<button ref={registerButtonRef} type="submit" disabled={error !== null}>
-					Зарегистрироваться
-				</button>
+				<button type="submit">Зарегистрироваться</button>
 			</form>
 		</div>
 	);
